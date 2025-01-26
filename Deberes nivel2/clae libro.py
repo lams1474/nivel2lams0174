fbce4f142c39793c801746e6ada910d1995d17eb
Lams1474 <la.maiguas@uea.edu.ec>
2025-01-26 12:13:16 -0500

    sube

---
+++ Deberes nivel2/clae libro.py
@@ -0,0 +1,17 @@
+class Persona:
+    def __init__(self, nombre, edad):
+        # Constructor: inicializa los atributos del objeto
+        self.nombre = nombre
+        self.edad = edad
+        print(f"Persona creada: {self.nombre}, {self.edad} años")
+
+    def __del__(self):
+        # Destructor: realiza limpieza o cierre de recursos
+        print(f"Persona eliminada: {self.nombre}")
+
+# Crear una instancia de la clase Persona
+persona1 = Persona("luis Maigua", 51)
+
+# Eliminar la instancia (esto llamará al destructor)
+del persona1
+
