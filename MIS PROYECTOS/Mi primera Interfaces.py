745f79a540c766a207ece6503a91c1918da2d20d
Lams1474 <la.maiguas@uea.edu.ec>
2025-04-02 15:05:09 -0500

    Merge remote-tracking branch 'github/master'  semana11

---
+++ MIS PROYECTOS/Mi primera Interfaces.py
@@ -0,0 +1,16 @@
+from tkinter import *
+
+raiz=Tk()
+
+raiz.title("Ventana Lams")
+
+raiz.resizable(True,False)
+
+raiz.iconbitmap("lams.ico")
+
+raiz.geometry("300x324")
+
+raiz.config(bg="blue")
+
+
+raiz.mainloop()
